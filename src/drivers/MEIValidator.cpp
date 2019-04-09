74102c39c1e3eae93eee0dcf3030490eb62b4540
mark <mark@mark@kyin.org>
2019-04-09 11:30:50 -0400

    CRC Commit befor lunch

--- src/drivers/MEIValidator.cpp
+++ src/drivers/MEIValidator.cpp
@@ -13,9 +13,9 @@ public:
 
 	int mei_do_crc(char buff[], int buffer_len){
 
-		 //printf("\n%02x%02x%02x%02x%02x%02x\n",buff[0],buff[1],buff[2],buff[3],buff[4],buff[5]);
+		 //printf("\n%02x%02x%02x%02x%02x%02x\n",buff[0],buff[1],buff[2],buff[3],buff[4],buff[5]); //DEBUG CODE
 
-		 //printf("%d\n",buffer_len);
+		 //printf("%d\n",buffer_len); //DEBUG CODE
 
 
 		 int i=1;
@@ -27,11 +27,11 @@ public:
 		        while ( i < buffer_len)
 		        {
 		           sum ^= buff[i];
-		           //printf("%02x,%02x\n",sum,buff[i]);
+		           //printf("%02x,%02x\n",sum,buff[i]); //DEBUG CODE
 		           i++;
 		        }
-		        //printf("%d\n",buffer_len);
-	            //printf("the check sum = %02x\n",sum);
+		        //printf("%d\n",buffer_len); //DEBUG CODE
+	            //printf("the check sum = %02x\n",sum); //DEBUG CODE
 
 		 return(sum);
 
