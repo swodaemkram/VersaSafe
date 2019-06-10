2833aecce5aa3194caea1e959252e086d16d7de9
Mark Meadows <mark@kyin.net>
2019-06-10 13:18:25 -0400

    moved mei_poll to mei_driver.class

--- src/drivers/mei_driver.h
+++ src/drivers/mei_driver.h
@@ -14,16 +14,41 @@ included by <termios.h> */
 #define FALSE 0
 #define TRUE 1
 
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <SerialStream.h>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <cstdint>
+#include <bits/stdc++.h>    // for strcpy
+#include <array>            // for array copy
+#include <iostream>
+#include <vector>
+#include <SerialPort.h>
+#include "../hdr/global.h"
+#include "../trim.inc"
+#include "../logs.inc"
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <termios.h>
+#include <fcntl.h>
+
 
 using namespace std;
+using namespace LibSerial;
 
 typedef struct
 {
 
 
 } mei_struc;
-
-
 
 
 #endif /* DRIVERS_MEI_DRIVER_H_ */
